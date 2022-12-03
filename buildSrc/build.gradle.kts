898d9609a6bb5df9217331a81012376dd4668ff3
johnyalam <johnyalam.lut@gmail.com>
2022-12-03 23:27:50 +0200

    Gradle dependency folder BuidSrc added.

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,9 @@
+//import org.gradle.kotlin.dsl.kotlin-dsl
+import org.gradle.kotlin.dsl.`kotlin-dsl`
+plugins {
+    "kotlin-dsl"
+}
+
+repositories {
+    mavenCentral()
+}
---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,15 @@
+/**
+ * To define plugins
+ */
+object BuildPlugins {
+    val android by lazy { "com.android.tools.build:gradle:${Versions.gradlePlugin}" }
+    val kotlin by lazy { "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}" }
+}
+
+/**
+ * To define dependencies
+ */
+object Deps {
+    val constraintLayout by lazy { "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}" }
+    val junit by lazy { "junit:junit:${Versions.jUnit}" }
+}
