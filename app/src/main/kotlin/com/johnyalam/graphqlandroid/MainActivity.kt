a29959de873b64b5bfd44d2d6cb72dd109fa443a
johnyalam <johnyalam.lut@gmail.com>
2022-12-05 21:59:57 +0200

    Text Modifier properties changed and background change.

--- app/src/main/kotlin/com/johnyalam/graphqlandroid/MainActivity.kt
+++ app/src/main/kotlin/com/johnyalam/graphqlandroid/MainActivity.kt
@@ -1,16 +1,27 @@
 package com.johnyalam.graphqlandroid
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.johnyalam.graphqlandroid.ui.JetpackComposeTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,7 +42,48 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Column( modifier = Modifier.padding(10.dp)) {
+        //Add Padding around message
+        Row(modifier = Modifier
+            .padding(all = 8.dp)
+            .background(color = Color.White)
+            .fillMaxWidth()){
+            Image(
+                painter = painterResource(R.drawable.person_image),
+                contentDescription = "Contact profile picture",
+                modifier = Modifier
+                    .size(100.dp)
+                    .padding(all = 8.dp)
+                    .clip(CircleShape)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Column(modifier = Modifier.height(100.dp).background(color = Color.White).fillMaxWidth()) {
+                Text(text = "Hello $name!", style =  TextStyle(color = Color.Black), fontSize = 24.sp)
+                Text(text = "Hello $name!", style = TextStyle(color = Color.Red), fontSize = 16.sp)
+            }
+        }
+
+        Row(modifier = Modifier
+            .padding(all = 8.dp)
+            .background(color = Color.White)
+            .fillMaxWidth()){
+            Image(
+                painter = painterResource(R.drawable.person_image),
+                contentDescription = "Contact profile picture",
+                modifier = Modifier
+                    .size(100.dp)
+                    .padding(all = 8.dp)
+                    .clip(CircleShape)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Column(modifier = Modifier.height(100.dp).background(color = Color.White)) {
+                Text(text = "Hello $name!", style =  TextStyle(color = Color.Black), fontSize = 24.sp)
+                Text(text = "Hello $name!", style = TextStyle(color = Color.Red), fontSize = 16.sp)
+            }
+        }
+    }
+
+
 }
 
 @Preview(showBackground = true)
@@ -41,3 +93,7 @@ fun DefaultPreview() {
         Greeting("Android")
     }
 }
+
+
+
+
--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,6 @@ plugins {
     id ("org.jetbrains.kotlin.android") version "1.7.10" apply false
 }
 
-tasks.register("clean", Delete::class){
-    delete(rootProject.buildDir)
-}
+//tasks.register("clean", Delete::class){
+//    delete(rootProject.buildDir)
+//}
